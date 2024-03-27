e35bccaeb29a7ac1e6e844ab9ff0c47e04569b25
Lysender <lysender@darkstar-win>
2024-03-27 10:06:52 +0800

    fix: more

--- hello.go
+++ hello.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"slices"
+	"time"
 )
 
 func main() {
@@ -10,6 +12,9 @@ func main() {
 	basics02()
 	basics03()
 	basics04()
+	basics05()
+	basics06()
+	basics07()
 }
 
 func basics01() {
@@ -45,11 +50,11 @@ func basics01() {
 }
 
 // Constants
-const s string = "constant"
+const someConstant string = "constant"
 
 func basics02() {
 	fmt.Println("------------")
-	fmt.Println(s)
+	fmt.Println(someConstant)
 
 	const n = 500000000
 
@@ -93,4 +98,150 @@ func basics03() {
 func basics04() {
 	fmt.Println("------------")
 
+	if 7%2 == 0 {
+		fmt.Println("7 is even (for some reason)")
+	} else {
+		fmt.Println("7 is odd (of course it is odd)")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 1 {
+		fmt.Println("either 8 or 7 are even")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
+
+func basics05() {
+	fmt.Println("------------")
+	i := 2
+	fmt.Print("Write ", i, " as ")
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend, he is going to sing or something")
+	default:
+		fmt.Println("It's a weekday!")
+	}
+
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
+
+func basics06() {
+	fmt.Println("------------")
+
+	var a [5]int
+
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var twoD [2][3]int
+	for x := 0; x < 2; x++ {
+		for y := 0; y < 3; y++ {
+			twoD[x][y] = x + y
+		}
+	}
+	fmt.Println("2d:", twoD)
+}
+
+func basics07() {
+	fmt.Println("------------")
+
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("append:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("copy:", c)
+
+	l := s[2:5]
+	fmt.Println("slice1:", l)
+
+	l = s[:5]
+	fmt.Println("slice2:", l)
+
+	l = s[2:]
+	fmt.Println("slice3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("declare:", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
 }
