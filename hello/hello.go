4e69ab3e309f1361b5e6ab330e37c51954a4213f
Lysender <lysender@darkstar-win>
2024-04-12 21:17:44 +0800

    fix: add a map example

--- hello/hello.go
+++ hello/hello.go
@@ -23,6 +23,7 @@ func main() {
 	basics09()
 
 	greetingsCall()
+	greetingsCallMulti()
 }
 
 func greetingsCall() {
@@ -41,8 +42,29 @@ func greetingsCall() {
 	}
 
 	// If no error was returned, print the returned message
-	// to tje console
+	// to the console
 	fmt.Println(message)
+}
+
+func greetingsCallMulti() {
+	// Set properties of the predefined Logger, incliding
+	// the long entry prefix and a flag to disable print
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Bob", "Alice", "Darwin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned map of
+	// messages to the console
+	fmt.Println(messages)
 }
 
 func basics01() {
